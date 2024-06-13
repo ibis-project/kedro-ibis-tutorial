a41acd74f5f4d161d8b1ccc458e0f7f0eccd8cde
Deepyaman Datta <deepyaman.datta@utexas.edu>
2024-06-13 22:18:07 +0000

    Upgrade Ibis (and remove patch) and recompile reqs

--- demo/delay-prediction/src/delay_prediction/__init__.py
+++ demo/delay-prediction/src/delay_prediction/__init__.py
@@ -2,36 +2,3 @@
 """
 
 __version__ = "0.1"
-
-
-def _auto_patch_ibis() -> None:
-    import ibis.backends.postgres.compiler
-    import ibis.common.exceptions as com
-
-    def visit_Hash(self, op, *, arg):
-        arg_dtype = op.arg.dtype
-
-        if arg_dtype.is_int16():
-            return self.f.hashint2extended(arg, 0)
-        elif arg_dtype.is_int32():
-            return self.f.hashint4extended(arg, 0)
-        elif arg_dtype.is_int64():
-            return self.f.hashint8extended(arg, 0)
-        elif arg_dtype.is_float32():
-            return self.f.hashfloat4extended(arg, 0)
-        elif arg_dtype.is_float64():
-            return self.f.hashfloat8extended(arg, 0)
-        elif arg_dtype.is_string():
-            return self.f.hashtextextended(arg, 0)
-        elif arg_dtype.is_macaddr():
-            return self.f.hashmacaddr8extended(arg, 0)
-
-        raise com.UnsupportedOperationError(
-            f"Hash({arg_dtype!r}) operation is not supported in the "
-            f"{self.dialect} backend"
-        )
-
-    ibis.backends.postgres.compiler.PostgresCompiler.visit_Hash = visit_Hash
-
-
-_auto_patch_ibis()
